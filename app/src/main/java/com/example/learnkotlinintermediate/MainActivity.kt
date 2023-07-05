6ee25334c9d234ac0755ea662aad7ecb649f1bec
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-07-05 18:29:29 +0700

    learn background thread

--- app/src/main/java/com/example/learnkotlinintermediate/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlinintermediate/MainActivity.kt
@@ -2,10 +2,89 @@ package com.example.learnkotlinintermediate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+        /*
+        //syncronous yang menyebabkan aplikasi menjadi freeze atau tidak responsif dan bisa menyebabkan aplikasi crash
+        btnStart.setOnClickListener {
+            try {
+                //simulate process compressing
+                for (i in 0..10) {
+                    Thread.sleep(500)
+                    val percentage = i * 10
+                    if (percentage == 100) {
+                        tvStatus.setText(R.string.task_completed)
+                    } else {
+                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                    }
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+         */
+
+        //asyncronous yang tidak menyebabkan aplikasi menjadi freeze atau tidak responsif dan tidak menyebabkan aplikasi crash
+        /*
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        btnStart.setOnClickListener {
+            executor.execute {
+                try {
+                    //simulate process in background thread
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+                        handler.post {
+                            //update ui in main thread
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+         */
+
+        //Coroutine cara yang lebih mudah untuk membuat asyncronous dan lebih ringan dari pada thread
+        btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        //update ui in main thread
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
